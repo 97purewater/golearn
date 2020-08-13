5f5e3718389681aade27e1ecb775191b0d014795
97purewater <379633105@qq.com>
2020-08-13 20:22:54 +0800

    测试修改

--- project06/fundemo01/main/main.go
+++ project06/fundemo01/main/main.go
@@ -1,10 +1,11 @@
 package main
+
 import (
 	"fmt"
-	"go_code/project06/fundemo01/util"
+	"project06/fundemo01/util"
 )
 
-func main()  {
+func main() {
 
 	result := util.Cal(12, 12, '+')
 	fmt.Println("result=", result)
